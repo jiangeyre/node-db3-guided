34fde0ac8f9f30a1e5ebd91e27676997ae71f9f1
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-22 10:02:17 -0800

    Commit 2

--- users/model.js
+++ users/model.js
@@ -2,11 +2,30 @@ const db = require('../data/db-config.js');
 
 // above the fold
 module.exports = {
-    list, 
+    list,
+    findByID,
+    insert, 
 };
 
 // implementation details
 function list() {
     // select * from users
     return db.select('*').from('users');
+    // return db('users'); // does the same thing
+};
+
+function findByID(userId) {
+    // select * from users where id = ?
+    return db('users')
+        .where({ id: userId })
+        .first();
+    // return db('users').where('id', userID);
+};
+
+function insert(user) {
+    return db('users')
+        .insert(user)
+        .then(([id]) => {
+            return findByID(id);
+        });
 };
--- users/user-router.js
+++ users/user-router.js
@@ -6,7 +6,7 @@ const Users = require('./model.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  Users.list()
+  Users.list() // <<<<< NEW
     .then(users => {
       res.json(users);
     })
@@ -15,34 +15,35 @@ router.get('/', (req, res) => {
     });
 });
 
+// move db logic to model file
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  db('users').where({ id })
-  .then(users => {
-    const user = users[0];
-
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'Could not find user with given id.' })
-    }
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to get user' });
-  });
+  Users.findByID(id) // <<<<<< NEW
+    .then(user => {
+      console.log("user", user);
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: 'Could not find user with given id.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get user' });
+    });
 });
 
+// move the post to the model
 router.post('/', (req, res) => {
   const userData = req.body;
 
-  db('users').insert(userData)
-  .then(ids => {
-    res.status(201).json({ created: ids[0] });
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to create new user' });
-  });
+  Users.insert(userData)
+    .then(created => {
+      res.status(201).json(created);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to create new user' });
+    });
 });
 
 router.put('/:id', (req, res) => {
